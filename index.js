1c417cf5e85defb13714bcd5cbbd1499944cb732
vrajlukhi <130537393+vrajlukhi@users.noreply.github.com>
2024-04-01 11:57:33 +0530

    crud

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const connect = require("./config/db");
-const app2 = require("./routes/userroute");
+const Route = require("./routes/userroute");
 const app = express();
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 app.set("view engine" , "ejs");
 app.set("views" + __dirname + "/views");
-app.use(app2)
+app.use(Route)
 app.listen(8090 , () =>{
     console.log("port is start 8090");
     connect();
